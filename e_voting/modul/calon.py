f37e96a74eb26c02e1ad2878b3fe768694f9fca5
djibriel <gol.d.jibriel@gmail.com>
2025-06-22 02:16:23 +0700

    Before Merge

--- e_voting/modul/calon.py
+++ e_voting/modul/calon.py
@@ -1,7 +1,7 @@
-calon=[{}]
+calon=[]
 def tambah_calon():#Struktur data: {"id": "CL001", "nama": "Budi", "visi": "Visi Misi", "jumlah_suara": 0}
-    id = input("Masukan ID")
-    nama = input("Masukan Nama")
-    visi = input("Masukan visi misi")
-    return calon.append({"id":id,"nama":nama,"visi":visi,"Jumlah_suara":0})
+    id = input("Masukan ID: ")
+    nama = input("Masukan Nama: ")
+    visi = input("Masukan visi misi: ")
+    return calon.append({"id":id,"nama":nama,"visi":visi,"jumlah_suara":0})
     
